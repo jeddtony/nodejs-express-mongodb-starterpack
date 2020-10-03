c75f32534192776667e7948bd87c2c00a9cb2d70
jeddtony <jedtony.jt@gmail.com>
2020-10-03 06:29:16 +0100

    Add reset password feature

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,9 @@ const router = require('../routes/userRoute');
 const UserRepository = require('../repository/userRepository')
 const {successResponse, errorResponse, notFoundResponse} = require('../helpers')
 const bcrypt = require('bcryptjs')
+const Mail = require('../mails');
+const {makeHash} = require('../helpers');
+
 
 exports.postLogin = async (req, res) => {
     let {email, password} = req.body
@@ -33,8 +36,60 @@ exports.postLogin = async (req, res) => {
 }
 
 
+exports.sendToken = async (req, res) => {
+    let {email} = req.body;
+
+    if(!email) {
+        return errorResponse(res, 'Email is required', {});
+    }
+
+    let token = generateToken(email);
+
+    let user = await UserRepository.storeToken(token, email);
+    if (!user) {
+        return errorResponse(res, 'Email does not exist', {email});
+    }
+
+    Mail.sendPasswordRest(user.token, user.email);
+    return successResponse(res, 'A reset password link was sent to your mail', {user})
+    
+}
+
+
+exports.resetPassword = async (req, res) => {
+    let token = req.params.token;
+    let {password} = req.body;
+
+    
+    // process.exit();
+
+    if(!password){ 
+        return errorResponse(res, 'Password is required', {});
+    }
+    let user = await UserRepository.getUser({token});
+    
+    if(!user) {
+        return errorResponse(res, 'This token is invalid')
+    }
+    try {
+        const isTokenValid = jwt.verify(token, process.env.JWT_TOKEN_SECRET);
+      let hashedPassword = await makeHash(password);
+      console.log('the hashed password ', hashedPassword);
+    //   process.exit();
+      let storedUser = await UserRepository.updatePassword(hashedPassword, user._id);
+      return successResponse(res, 'Password Updated', {storedUser});
+
+    } catch (err){
+        res.status(400).send('Invalid Token');
+    }
+}
 
 const comparePassword = async (password, hashedPassword) => {
     const validPass = await bcrypt.compare(password, hashedPassword);
     return validPass
+}
+
+const generateToken =  (email) => {
+    let token = jwt.sign({email}, process.env.JWT_TOKEN_SECRET, { expiresIn: '3h' });
+    return token
 }
